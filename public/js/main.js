0f033af5306e2be9544cc7221e68963746b6004b
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-22 15:03:49 +0100

    ex completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
 const myApp = new Vue({
     el: '#app',
     data: {
-       
+       mailingList: []
+    },
+    mounted() {
+       let self = this;
+
+       for (let i = 0; i < 10; i++) {
+         axios
+         .get('https://flynn.boolean.careers/exercises/api/random/mail')
+         .then((response) => {
+            if (response.data.success) {
+                 self.mailingList.push(response.data.response);
+            }
+         });
+       }
+
+      //  console.log(self.mailingList);
     }
 });
